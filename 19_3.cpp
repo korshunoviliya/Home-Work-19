596a82a855acd66258085f2a824d091dfc3755eb
korshunov80@mail.ru <korshunov80@mail.ru>
2021-03-11 09:17:38 +0300

    Update 19_3.cpp

--- 19_3.cpp
+++ 19_3.cpp
@@ -22,7 +22,7 @@ int main() {
     temp >> num1 >> action >> num2;
 
     if (num2 == 0 && action == '/') std::cerr << "Can't divide by zero";
-    else if (action != '+' || action != '-' || action != '*' || action != '/') std::cerr << "Unknown action";
+    else if (action != '+' && action != '-' && action != '*' && action != '/') std::cerr << "Unknown action";
     else {
         double result = totalResult (num1, action, num2);
         std::cout << num1 << " " << action << " " << num2 << " = " << result << std::endl;
